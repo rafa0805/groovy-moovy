05a1f6be9fefd202fb9937a04329b7e82d2eb3c4
vagrant <vagrant@localhost.localdomain>
2020-06-25 06:05:24 +0900

    change db info 3

--- config/config.php
+++ config/config.php
@@ -1,7 +1,7 @@
 <?php
 ini_set('display_errors', 1);
 
-define('DSN', 'mysql:dbhost=us-cdbr-east-05.cleardb.net;dbname=heroku_7bc3b6edd26e187');
+define('DSN', 'mysql:host=us-cdbr-east-05.cleardb.net;dbname=heroku_7bc3b6edd26e187');
 define('DB_USERNAME', 'b6d8d042fe52ce');
 define('DB_PASSWORD', '72076159');
 define('SITE_URL', $_SERVER['HTTP_HOST']);
@@ -10,5 +10,3 @@ require_once(__DIR__ . '/autoload.php');
 require_once(__DIR__ . '/../app/functions.php');
 
 session_start();
-
-
