688e4ff36afe5823e9d7370ee3b30c00f084344f
vagrant <vagrant@localhost.localdomain>
2020-06-25 05:37:17 +0900

    changed db info

--- config/config.php
+++ config/config.php
@@ -1,15 +1,14 @@
 <?php
 ini_set('display_errors', 1);
 
-define('DSN', 'mysql:dbhost=localhost;dbname=groovymoovy_db');
-define('DB_USERNAME', 'app_user');
-define('DB_PASSWORD', 'aaaaaa');
+define('DSN', 'mysql:dbhost=us-cdbr-east-05.cleardb.net;dbname=groovy_moovy');
+define('DB_USERNAME', 'b6d8d042fe52ce');
+define('DB_PASSWORD', '72076159');
 define('SITE_URL', $_SERVER['HTTP_HOST']);
 
 require_once(__DIR__ . '/autoload.php');
 require_once(__DIR__ . '/../app/functions.php');
 
 session_start();
-
 
 
